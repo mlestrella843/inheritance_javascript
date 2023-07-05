ccf71e10fb3e2ee8db99a02406f5617279e694f9
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-05 10:03:01 -0600

    Practicing getters y setters

--- index2.js
+++ index2.js
@@ -24,3 +24,24 @@ let deportista1 = new Deportista("Maria", "Estrella");
 //Instaciamos un futbolista
 
 let futbolista1 = new Futbolista("Juan", "Pereyra", 5);
+
+
+//TRabajando con getters y setters, estos se usan cuando las propiedades son privadas
+//las cuales se especifican con un guion delante
+
+class Tenista extends Deportista{
+    constructor(nombre, apellido, sets){
+        super(nombre, apellido); // con la palabra super estoy diciendo que esos parametros los heredo de la clase base.
+        this._sets = sets;
+    }
+    //Usamos get para accesar la variable privada sets
+    get sets(){
+        return this._sets;
+    }
+    set sets(nuevoSet){
+        return this._sets = nuevoSet;
+    }
+}
+
+let tenista1 = new Tenista("Maria", "Estrella", 3);
+
